5fc3386780c408ff92cdd29a85edb44b0e5af55d
GuilhermeFaria <guilhermefaguiar@gmail.com>
2023-09-11 14:43:34 -0300

    Ultimas modificações

--- src/components/header.js
+++ src/components/header.js
@@ -10,9 +10,9 @@ export default function Header(){
             <div class="container">
                 <ul class="header_menu_list">
                     <img class="logoporto" src={logoporto} alt="Logotipo da porto"/>
-                    <li class="headermenu-listitem" onClick={() => {navegação('/')}}>Vistoria</li>
-                    <li class="headermenu-listitem" onClick={() => {navegação('/enviofotos')}}>Fotos</li>
-                    <li class="headermenu-listitem" onClick={() => {navegação('/integrantes')}}>Integrantes</li>
+                    <li class="header__menu-list__item" onClick={() => {navegação('/')}}>Vistoria</li>
+                    <li class="header__menu-list__item" onClick={() => {navegação('/enviofotos')}}>Fotos</li>
+                    <li class="header__menu-list__item" onClick={() => {navegação('/integrantes')}}>Integrantes</li>
                     <img class="usu-icon" src={icon} alt="Ícone de usuário"/>
                 </ul>
             </div>
