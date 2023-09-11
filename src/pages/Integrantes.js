dee1b9cc941f2051f79f6988ce66604b813cb7fb
GuilhermeFaria <guilhermefaguiar@gmail.com>
2023-09-11 14:05:04 -0300

    Criando a page Integrantes

---
+++ src/pages/Integrantes.js
@@ -0,0 +1,22 @@
+import '../style.css';
+
+
+export default function Integrantes() {
+    return (
+        <main>
+            <div class="titulo-subtitulo">
+                <h1 class="titulo">INTEGRANTES</h1>
+                <h3 class="subtitulo">Bruno Ramos da Costa - RM551942</h3>
+                <h3 class="subtitulo">Guilherme Faria  - RM551374</h3>
+                <h3 class="subtitulo">Henrique Roncon  - RM99161</h3>
+                <h3 class="subtitulo">Lucas Carabolad Bob  - RM550519</h3>
+                <h3 class="subtitulo">Paulo Henrique  - RM99704</h3>
+
+                <div class="titulo-subtitulo">
+                    <h1 class="titulo">REPOSITÓRIO</h1><br/>
+                        <a class="subtitulo" href="www.github.com">Link do projeto - Clique aqui!</a>
+                </div>
+            </div>
+        </main>
+    )
+}
